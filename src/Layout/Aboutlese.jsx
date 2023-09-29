c096395c05a15bd38c04d2844a776fe70e3fcbe0
ogel <yocorangelo18@gmail.com>
2023-09-29 16:38:38 +0800

    fix gif

--- src/Layout/Aboutlese.jsx
+++ src/Layout/Aboutlese.jsx
@@ -5,7 +5,7 @@ export const Aboutlese = () => {
    <div className="bg-[#535B69] h-auto w-auto grid place-items-center overflow-hidden ">
      <div className="break-words  grid place-items-center  p-9" id="lesepad">
      <h1 className="text-9xl break-words font-black" id="LESSERAFIM">WHO ARE <span className="text-[#950101]">LESSERAFIM</span></h1>
-     <p className="text-2xl break-words ">
+     <p className="text-2xl break-words text-white ">
 LE SSERAFIM is a five-member South Korean girl group under HYBE Corporation, consisting of members Sakura, Kim Chaewon, Huh Yunjin, Kazuha, and Hong Eunchae. They debuted on May 2, 2022, with the release of their first extended play, Fearless. The group's name is an anagram of the phrase "I'm Fearless".
 
 LE SSERAFIM has been praised for their strong vocals, powerful dance performances, and unique musical concept. They have quickly become one of the most popular K-pop girl groups of the fourth generation.</p>
